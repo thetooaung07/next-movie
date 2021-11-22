d230280520510fef1707529fc16f200120fc163c
thetooaung07 <thetooaung07@gmail.com>
2021-11-22 20:46:39 +0630

    Add TMDB Api > fetch movies and shows

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,8 @@
-// import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google"
-// import { FirebaseAdapter } from "@next-auth/firebase-adapter"
-
-// import firebase from "firebase/app"
-// import "firebase/firestore"
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+// import { initializeApp, getApp, getApps } from 'firebase/app'
+// import { getFirestore } from 'firebase/firestore'
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyBSW84y5CN5nO58LGRLJq1V3EU97Qk26n8",
@@ -13,20 +12,19 @@
 //   messagingSenderId: "623738648258",
 //   appId: "1:623738648258:web:0a1b319197a761ddc86d5b",
 // };
-// const firestore = (
-//   firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
-// ).firestore()
+
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const db = getFirestore(app);
 
 // // For more information on each option (and a full list of options) go to
 // // https://next-auth.js.org/configuration/options
 // export default NextAuth({
-//   // https://next-auth.js.org/configuration/providers
+//   // https://next-auth.js.org/providers/overview
 //   providers: [
 //     GoogleProvider({
 //       clientId: process.env.GOOGLE_ID,
 //       clientSecret: process.env.GOOGLE_SECRET,
 //     }),
 //   ],
-//   adapter: FirebaseAdapter(firestore),
-  
-// })
+//   adapter: FirebaseAdapter(db),
+// });
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import MoviesCollection from "../components/MoviesCollection";
+import ShowsCollection from "../components/ShowsCollection";
 import Slider from "../components/Slider";
 import StudioCard from "../components/StudioCard";
 
-export default function Home() {
+export default function Home({
+  popularMovies,
+  popularShows,
+  topRatedMovies,
+  topRatedShows,
+}) {
+  console.log(popularMovies)
   return (
     <div className="">
       <Head>
@@ -14,10 +22,53 @@ export default function Home() {
       <Header></Header>
       {/* <Hero/> */}
       <main>
-         <Slider/>
-      <StudioCard/>
+        <Slider />
+        <StudioCard />
+        <MoviesCollection results= {popularMovies} title="Popular Movies" />
+        <ShowsCollection results={popularShows} title="Popular Shows" />
+        <MoviesCollection results= {topRatedMovies} title="Top Rated Movies" />
+        <ShowsCollection results={topRatedShows} title="Top Rated Shows" />
       </main>
-     
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // const session = await getSession(context)
+
+  const [
+    popularMoviesRes,
+    popularShowsRes,
+    topRatedMoviesRes,
+    topRatedShowsRes,
+  ] = await Promise.all([
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+    fetch(
+      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+    fetch(`
+    https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`),
+    fetch(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+  ]);
+  const [popularMovies, popularShows, topRatedMovies, topRatedShows] =
+    await Promise.all([
+      popularMoviesRes.json(),
+      popularShowsRes.json(),
+      topRatedMoviesRes.json(),
+      topRatedShowsRes.json(),
+    ]);
+
+  return {
+    props: {
+      // session,
+      popularMovies: popularMovies.results,
+      popularShows: popularShows.results,
+      topRatedMovies: topRatedMovies.results,
+      topRatedShows: topRatedShows.results,
+    },
+  };
 }
